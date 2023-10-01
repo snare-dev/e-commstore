1734d06c8478ec2342c134aff8dcd3524b6a4ac0
snare-dev <amanisanare1@gmail.com>
2023-10-01 11:38:35 -0400

    second commit

---
+++ src/pages/HomeLayout.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+
+const HomeLayout = () => {
+  return (
+    <>
+      <nav> 
+        <span className="text-4xl text-primary">Snare</span>
+      </nav>
+      <Outlet />
+    </>
+  );
+};
+
+export default HomeLayout;
